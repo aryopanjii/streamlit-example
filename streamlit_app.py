594eb09d2bd71a39e31e48053dcb1be7840bb3f5
aryopanjii <aryopanjii@gmail.com>
2022-10-06 20:02:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,45 +14,64 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
-
-
-st.write(1234)
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 2000, 3000, 40000],
-}))
-
-
-apikey = 'K8AF2wV5T0z1Pi6YHFFiOvLPnY5MdAKtjIN8AfRfTEFCXzsF8nxC38vwyD6yTkeV'
-secret = 'Aryomadan123'
-#!pip install python-binance pandas mplfinance
-
-#Import library
-from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
+import streamlit as st
 import pandas as pd
 
-client = Client(apikey, secret)
-tickers = client.get_all_tickers()
+st.markdown('''# **Binance Price App**
+A simple cryptocurrency price app pulling price data from *Binance API*.
+''')
 
-ticker_df = pd.DataFrame(tickers)
+st.header('**Selected Price**')
 
-ticker_df_symbol = ticker_df.drop(columns='price', axis=1)
-ticker_df_symbol
+# Load market data from Binance API
+df = pd.read_json('https://api.binance.com/api/v3/ticker/24hr')
 
-historical = client.get_historical_klines('BTCUSDT', Client.KLINE_INTERVAL_1DAY, '1 Jan 2011')
-hist_df_LIT = pd.DataFrame(historical)
-hist_df_LIT.columns = ['Open Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close Time', 'Quote Asset Volume', 
-                    'Number of Trades', 'TB Base Volume', 'TB Quote Volume', 'Ignore']
+# Custom function for rounding values
+def round_value(input_value):
+    if input_value.values > 1:
+        a = float(round(input_value, 2))
+    else:
+        a = float(round(input_value, 8))
+    return a
 
-hist_df_LIT['Open Time'] = pd.to_datetime(hist_df_LIT['Open Time']/1000, unit='s')
+crpytoList = {
+    'Price 1': 'BTCBUSD',
+    'Price 2': 'ETHBUSD',
+    'Price 3': 'BNBBUSD',
+    'Price 4': 'XRPBUSD',
+    'Price 5': 'ADABUSD',
+    'Price 6': 'DOGEBUSD',
+    'Price 7': 'SHIBBUSD',
+    'Price 8': 'DOTBUSD',
+    'Price 9': 'MATICBUSD'
+}
 
-hist_df_LIT.rename(columns = {"Open Time":"Date"}, inplace=True)
-hist_df_LIT.rename(columns = {"Open Time":"Date"}, inplace=True)
-hist_df_LIT = hist_df_LIT.set_index('Date')
-#hist_df_LIT = hist_df_LIT[['Close']]
+col1, col2, col3 = st.columns(3)
 
-#hist_df_LIT.drop(columns='Unnamed: 0', inplace=True)
-hist_df_LIT.drop(columns='Ignore', inplace=True)
-#hist_df_LIT.rename(columns = {'Close':'Close_LIT'}, inplace=True)
-hist_df_LIT = hist_df_LIT
-hist_df_LIT
+for i in range(len(crpytoList.keys())):
+    selected_crypto_label = list(crpytoList.keys())[i]
+    selected_crypto_index = list(df.symbol).index(crpytoList[selected_crypto_label])
+    selected_crypto = st.sidebar.selectbox(selected_crypto_label, df.symbol, selected_crypto_index, key = str(i))
+    col_df = df[df.symbol == selected_crypto]
+    col_price = round_value(col_df.weightedAvgPrice)
+    col_percent = f'{float(col_df.priceChangePercent)}%'
+    if i < 3:
+        with col1:
+            st.metric(selected_crypto, col_price, col_percent)
+    if 2 < i < 6:
+        with col2:
+            st.metric(selected_crypto, col_price, col_percent)
+    if i > 5:
+        with col3:
+            st.metric(selected_crypto, col_price, col_percent)
+
+st.header('**All Price**')
+st.dataframe(df)
+
+st.info('Credit: Created by Chanin Nantasenamat (aka [Data Professor](https://youtube.com/dataprofessor/))')
+
+st.markdown("""
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+""", unsafe_allow_html=True)
