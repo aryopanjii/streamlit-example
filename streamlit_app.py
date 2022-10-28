337bc22da23b742a9a92e2dec29ea79c23828950
aryopanjii <aryopanjii@gmail.com>
2022-10-28 16:36:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,13 @@
 import pandas as pd
 import streamlit as st
-import numpy as np
-
-import numpy as np
-import time as tm
-import datetime as dt
-import tensorflow as tf
-
-# Data preparation
-from yahoo_fin import stock_info as yf
-#from sklearn.preprocessing import MinMaxScaler
-#from collections import deque
-
-# AI
-#from keras.models import Sequential
-#from keras.layers import Dense, LSTM, Dropout
-
-# Graphics library
-import matplotlib.pyplot as plt
 
 st.title("Welcome to Streamlit!")
 
+st.write("Our first DataFrame")
 
-# SETTINGS
+st.write(
+  pd.DataFrame({
+      'A': [1, 2, 3, 4],
+      'B': [5, 6, 7, 8]
+    })
+)
