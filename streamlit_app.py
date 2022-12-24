ee0ee6cac8daf36fb4268c52895dd7fce83b3126
aryopanjii <aryopanjii@gmail.com>
2022-12-24 18:07:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,5 @@
 from collections import namedtuple
-
 import altair as alt
-
 import math
-
 import pandas as pd
-
 import streamlit as st
-
-
-"""
-
-# Welcome to Young Trader indo apps!
-
-this is app v.1 for showing mark price from API Binance on realtime price update no delay.
-
-stream data success using streamlit framework
-
-
-
-v.2 --> Creating modeling for learn everytime and learn everysecond price movement on binance using random forest classification (supervised learning)
